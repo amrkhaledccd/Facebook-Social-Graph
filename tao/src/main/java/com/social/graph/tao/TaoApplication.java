270ec7e69a44bd3560c0969ab253d6b4cf1000d0
swapyinc <swapyinc@gmail.com>
2021-09-29 01:54:00 +0200

    startup project

---
+++ tao/src/main/java/com/social/graph/tao/TaoApplication.java
@@ -0,0 +1,13 @@
+package com.social.graph.tao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaoApplication.class, args);
+	}
+
+}
---
+++ tao/src/test/java/com/social/graph/tao/TaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.graph.tao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
